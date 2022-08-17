02fd06f3bb5bbad5b89023af856e5527d55a04a2
Napol Rachatasumrit <naplr42@gmail.com>
2022-08-17 18:27:20 +0700

    Remove unused files and add react solutions

--- README.md
+++
@@ -1,1 +0,0 @@
-# teach-pui-example-react
---
+++ solution-hw1/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Solution to HW1
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
